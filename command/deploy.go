ffe7db674122ab5584f5ceda7a1bd3575f040e94
Valentin Ouvrard <valentin.ouvrard@nautile.sarl>
2017-06-30 21:54:29 +1100

    clean comments

--- command/deploy.go
+++ command/deploy.go
@@ -5,4 +5,17 @@ import "github.com/codegangsta/cli"
 func CmdDeploy(c *cli.Context) {
 	// Write your code here
 
+	// Check config json
+
+	// Check jq
+
+	// Check docker-compose
+
+	// Check docker run
+
+	// Check args and set variables (localhost/dev/integration/master)
+
+	// Set all config from json
+
+	// Deploy stack
 }
---
+++ command/global.go
@@ -0,0 +1,77 @@
+package command
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/jmoiron/jsonq"
+	"github.com/tbruyelle/hipchat-go/hipchat"
+)
+
+// Check error
+func Check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+// Exec shell command
+func Run(command string) {
+	args := strings.Split(command, " ")
+	cmd := exec.Command(args[0], args[1:]...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("command failed: %s\n", command)
+		panic(err)
+	}
+}
+
+// Exec shell command muted
+func RunMuted(command string) {
+	args := strings.Split(command, " ")
+	cmd := exec.Command(args[0], args[1:]...)
+	//cmd.Stdout = os.Stdout
+	//cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("command failed: %s\n", command)
+		panic(err)
+	}
+}
+
+// Check if a file exist
+func Exists(name string) bool {
+	_, err := os.Stat(name)
+	return !os.IsNotExist(err)
+}
+
+// Notify Hipchat room (value from config.json)
+func HipchatNotify(message string) bool {
+	room := getConfigKey("hipchatRoom")
+	token := getConfigKey("hipchatToken")
+	c := hipchat.NewClient(token)
+	notifRq := &hipchat.NotificationRequest{Message: message}
+	_, err := c.Room.Notification(room, notifRq)
+	if err != nil {
+		panic(err)
+	}
+	return true
+}
+
+func getConfigKey(configKey string) string {
+	ConfigPath := "config/env-config.json"
+	b, err := ioutil.ReadFile(ConfigPath) // just pass the file name
+	Check(err)
+	str := string(b) // convert content to a 'string'
+	data := map[string]interface{}{}
+	dec := json.NewDecoder(strings.NewReader(str))
+	dec.Decode(&data)
+	jq := jsonq.NewQuery(data)
+	brutJson, err := jq.String("config", configKey)
+	configKeyStr := string(brutJson)
+	return configKeyStr
+}

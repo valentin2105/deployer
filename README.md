e4007e4c9eaa027d0f648278fec885a3bd8dce29
Valentin Ouvrard <valentin.ouvrard@nautile.sarl>
2017-06-30 21:42:18 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# deployer
+
+
+
+## Description
+
+## Usage
+
+## Install
+
+To install, use `go get`:
+
+```bash
+$ go get -d github.com/Valentin Ouvrard/deployer
+```
+
+## Contribution
+
+1. Fork ([https://github.com/Valentin Ouvrard/deployer/fork](https://github.com/Valentin Ouvrard/deployer/fork))
+1. Create a feature branch
+1. Commit your changes
+1. Rebase your local changes against the master branch
+1. Run test suite with the `go test ./...` command and confirm that it passes
+1. Run `gofmt -s`
+1. Create a new Pull Request
+
+## Author
+
+[Valentin Ouvrard](https://github.com/Valentin Ouvrard)
---
+++ commands.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"fabric.git.nautile.tech/publicrepo/deployer/command"
+	"github.com/codegangsta/cli"
+)
+
+var GlobalFlags = []cli.Flag{}
+
+var Commands = []cli.Command{
+	{
+		Name:   "deploy",
+		Usage:  "",
+		Action: command.CmdDeploy,
+		Flags:  []cli.Flag{},
+	},
+	{
+		Name:   "delete",
+		Usage:  "",
+		Action: command.CmdDelete,
+		Flags:  []cli.Flag{},
+	},
+	{
+		Name:   "list",
+		Usage:  "",
+		Action: command.CmdList,
+		Flags:  []cli.Flag{},
+	},
+}
+
+func CommandNotFound(c *cli.Context, command string) {
+	fmt.Fprintf(os.Stderr, "%s: '%s' is not a %s command. See '%s --help'.", c.App.Name, command, c.App.Name, c.App.Name)
+	os.Exit(2)
+}

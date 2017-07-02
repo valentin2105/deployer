72ff0fd560c0a4c4ecedae3f63ae4ac41f7f9f7f
Valentin Ouvrard <valentin.ouvrard@nautile.sarl>
2017-07-02 16:46:36 +1100

    tests

--- command/global.go
+++ command/global.go
@@ -43,10 +43,10 @@ func RunMuted(command string) {
 	args := strings.Split(command, " ")
 	cmd := exec.Command(args[0], args[1:]...)
 	//cmd.Stdout = os.Stdout
-	//cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
 		fmt.Printf("command failed: %s\n", command)
-		fmt.Println(os.Stderr)
+		cmd.Stderr = os.Stderr
+		fmt.Println(err)
 		os.Exit(1)
 	}
 }

037692459cac46c6d9467996931207bbfb2ec617
Valentin Ouvrard <valentin.ouvrard@nautile.sarl>
2017-07-11 14:39:03 +1100

    list fixed.

--- command/list.go
+++ command/list.go
@@ -11,5 +11,5 @@ func CmdList(c *cli.Context) {
 	environmentPassed := os.Args[2]
 	stackPath := fmt.Sprintf(".generated/%s.yml", environmentPassed)
 	cmdList := fmt.Sprintf("docker-compose -f %s ps", stackPath)
-	RunMuted(cmdList)
+	Run(cmdList)
 }
